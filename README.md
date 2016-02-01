860befe74b3b44aacefe0ac9c3fd0298f09dad24
Antonio Cao <antoniocao000@gmail.com>
2016-02-01 13:41:25 +0100

    Fluxbox Update

--- README.md
+++
@@ -1,2 +0,0 @@
-# fluxbox
-Fluxbox clone ;)
---
+++ nls/tr_TR/generated-ISO-8859-9.m
@@ -0,0 +1,282 @@
+$ codeset=ISO-8859-9
+
+$set 1 #Align
+
+1 Alt Orta
+2 Alt Sol
+3 Alt Sað
+4 Yatay
+5 Sol
+6 Sol Alt
+7 Sol Orta
+8 Sol Üst
+9 Baðlý
+10 Sað
+11 Sað Alt
+12 Sað Orta
+13 Sað üst
+14 Üst Orta
+15 Üst Sol
+16 Üst Sað
+17 Dikey
+18 Orta
+19 Üst
+20 Alt
+
+$set 2 #BaseDisplay
+
+1 Ýptal Ediliyor... çekirdek dökümü alýnýyor\n
+2 Kapatýlýyor\n
+3 %s:      %d sinyali alýndý\n
+
+$set 3 #Common
+
+1 Alfa
+2 Otomatik Gizle
+3 Derlenmiþ
+4 Derlenmiþ ayarlar
+5 Derleyici
+6 Derleyici versiyonu
+7      baþlat
+8      tuþlar
+9         nls
+10        menü
+20  windowmenu
+11        stil
+12 Varsayýlanlar
+13 devre dýþý
+14 Hata
+15 Fluxbox sürümü
+16 En Üste Al
+17 GIT Revizyonu
+18 Görünür
+19 Bu biçemde tanýmlanmýþ arkaplan seçeneði yok.\nLütfen kýlavuzu ya da SSS'yi okuyun.
+
+$set 4 #Configmenu
+
+1 Kenar Düzleþtirme
+2 Otomatik Büyütme
+3 Týklayarak Büyütme
+4 Odaklamak için Týkla
+5 Geçici Pencereleri Süsle
+6 Masaüstleri Arasý Fare Tekerleði ile Geçiþ
+7 Çalýþma Alaný Deðiþtiðinde Pencereye Odaklan
+8 Kalýba Odakla
+9 Yeni Pencereye Odakla
+10 Tamamen Büyütme
+11 Resim Titretme
+12 Opak Pencere Taþýma
+14 Fare Odaðý
+15 Çalýþma Alaný Sapmasý
+16 Sahte Saydamlýk için Zorla
+17 Menü Alfasý
+18 Saydamlýk
+19 Odaklanýlmýþ Pencere Alfasý
+20 Odaklanýlmamýþ Pencere Alfasý
+21 Sekme Özellikleri
+22 Baþlýk Çubuðunda Sekmeler
+23 Harici Sekme Geniþliði
+24 Týklama Sekmesi Odaðý
+25 Fare Sekmesi Odaðý
+26 Ayarlarý En Üst Düzeye Getir
+27 Boyutlandýrma Arttýrmasýný Görmezden Gel
+28 Taþýmayý Devre Dýþý Býrak
+29 Boyutlandýrmayý Devre Dýþý Býrak
+
+$set 5 #Ewmh
+
+1 Ölümcül: Bellek Yetersiz, EWMH istemci listesi için ayrýlamýyor
+
+$set 6 #FbTkError
+
+1 Uyarý! Boy > 3200, Boy 3200 olarak ayarlanýyor
+2 Uyarý! En > 3200, En 3200 olarak ayarlanýyor
+3 Uyarý! ikincil yazý tipi yüklenemiyor
+4 Ayýrma hatasý.
+5 Pixmap oluþtururken Hata
+6 XImage oluþturulamýyor
+7 Okunamýyor
+8 Uyarý: Alfa için uygun biçim bulunamadý.
+9 Uyarý: Alfa resmi oluþturulamadý.
+10 Uyarý: Alfa pixmap'ý oluþturulamadý.
+11 Ekran(%d) için biçim bulunamadý
+12 Bellek yetersiz
+13 Mavi tampon için bellek ayrýlýrken bellek yetersiz.
+14 Yeþil tampon için bellek yarýlýrken bellek yetersiz.
+15 Kýrmýzý tampon için bellek ayrýlýrken bellek yetersiz.
+16 Tema ögesi okunamadý
+17 desteklenmeyen görsel
+18 Varsayýlan deðer ayarlanýyor
+
+$set 7 #Fluxbox
+
+1 rc dosya adý geçersiz!
+2 Groupfile yüklenemedi
+3 Veritabaný yüklenemedi
+4 Tekrar deneniyor:
+5 Uyarý! Pencereleri eþleyecek ekran bulunamýyor!
+6 %s dizini oluþturulamýyor
+7 Düzenlenecek pencereler bulunamýyor.\nBaþka bir pencere yöneticisinin çalýþmadýðýndan emin olun.
+8 Düzenli ifadeler ayrýþtýrýlýrken hata
+9 Ölümcül! Fluxbox sýnýflarýndan sadece bir örnek olabilir.
+10 X sunucusuna baðlanýlamýyor.\nFluxbox'u baþlatmadan önce X'i baþlattýðýnýzdan emin olun.
+11 Uyarý: X sunucusu yerelleþtirmeyi desteklemiyor
+12 Uyarý: Yerelleþtirme tanýmlayýcýlarý ayarlanamýyor
+
+$set 8 #Gnome
+
+1 Ölümcül: Bellek yetersiz, GNOME istemci listesi için yer ayrýlamýyor
+
+$set 9 #Keys
+
+1 Tuþlar: Satýrda hata
+2 Tuþlar: Tuþ aðacý birleþtirilirken hata!
+3 Tuþlar: Satýrda geçersiz tuþ/tanýmlayýcý
+
+$set 10 #Menu
+
+1 Yapýlandýrma
+2 Fluxbox varsayýlan menü
+3 Çýkýþ
+4 Simgeler
+5 Katman...
+6 En Baþta...
+7 Yerleþtirme
+8 Yapýlandýrmayý Yeniden Yükle
+9 Yeniden baþlat
+10 Uyarý: dengesiz [þifreleme] etiketleri
+
+$set 11 #Remember
+
+1 Süslemeler
+2 Boyutlar
+3 Çalýþma alanýna atla
+4 Katman
+5 Hatýrla...
+6 Konum
+7 Çýkýþta kaydet
+8 Gölgelenmiþ
+9 Yapýþkan
+10 Bilinmeyen uygulama tuþu
+11 Çalýþma Alaný
+12 Baþ
+13 Saydamlýk
+
+$set 12 #Screen
+
+1 BScreen::BScreen: X sunucusu sorgulanýrken hata oluþtu.\n	þu an ekranda baþka bir pencere yöneticisi çalýþýyor
+2 E: %4d x B: %4d
+3 BScreen::BScreen: %d ekraný düzenleniyor 0x%lx görseli, %d derinliði kullanýlýyor\n
+4 E: %04d x B: %04d
+
+$set 13 #Slit
+
+1 Ýstemciler
+2 Devir aþaðý
+3 Devir yukarý
+4 Kesit Yönü:
+5 Kesit Katmaný
+6 Kesit Tepede
+7 Kesit Yerleþimi
+8 Kesit
+9 Kesit Listesini kaydet
+
+$set 14 #Toolbar
+
+1 Geçerli çalýþma alanýný adlandýr
+2 Simge Çubuðu Kipi
+3 Tüm Pencereler
+4 Simgeler
+5 Hiç
+6 Çalýþma Alaný
+7 Çalýþma Alaný Simgeleri
+8 Araç Çubuðu Katmaný
+9 Araç Çubuðu En Üstte
+10 Araç Çubuðu Yerleþtirmesi
+11 Araç Çubuðu
+12 Araç çubuðu geniþlik yüzdesi
+13 Saat: 24s
+14 Saat: 12s
+15 Saat Biçimini Düzenle
+16 Resimleri Göster
+17 SimgelerYok
+18 ÇalýþmaAlanýSimgelerYok
+
+$set 15 #Window
+
+1 Ýsimsiz
+
+$set 16 #Windowmenu
+
+1 Kapat
+2 Simge boyutuna küçült
+3 Katman
+4 Düþür
+5 Büyüt
+6 Yükselt
+7 Þuraya Gönder ...
+8 Gölge
+9 Yapýþ
+10 Öldür
+11 Varsayýlanlarý Kullan
+12 Pencere Baþlýðýný Ayarla
+
+$set 17 #Workspace
+
+1 Çalýþma Alaný %d
+2 Çalýþma Alanlarý
+3 Yeni Çalýþma Alaný
+4 Sonuncuyu Sil
+
+$set 18 #fbsetroot
+
+1 Hata: Bunlardan en az birini belirtmelisiniz: -solid, -mod, -gradient\n
+2 pixmap atomlarý oluþturulamýyor, býrakýlýyor!
+3 -display <string>        baðlantýyý görüntüle\n\
+-mod <x> <y>             mod kalýbý\n\
+-foreground, -fg <color> mod önplan rengi\n\
+-background, -bg <color> mod arkaplan rengi\n\n\
+-gradient <texture>      doku eðimi\n\
+-from <color>            baþlangýç renk eðimi\n\
+-to <color>              bitiþ renk eðimi\n\n\
+-solid <color>           rengi katýlaþtýrýr\n\n\
+-help                    bu yardým ekranýný görüntüle ve çýk\n
+
+$set 19 #main
+
+1 hata: '-display' bir parametre ile birlikte kullanýlýyor
+2 Kötü Ayýrma
+3 Kötü biçim
+4 Aralýk dýþý
+5 Çalýþma zamaný hatasý
+6 Standart Ýstisna
+7 Bilinmeyen hata
+8 hata: '-log' bir parametre ile birlikte kullanýlýyor
+9 Günlük Dosyasý
+10 Günlük tutuluyor: 
+11 hata: '-rc' bir parametre ile birlikte kullanýlýyor
+12 hata: '-screen' bir parametre ile birlikte kullanýlýyor
+13 Fluxbox %s: (c) %s Fluxbox Takýmý\n\
+Web sitesi: http://www.fluxbox.org/\n\n\
+-display <string>\t\tekran baðlantýsýný kullan.\n\
+-screen <all|int,int,int>\tsadece belirlenen ekranlarda çalýþ.\n\
+-no-slit\t\t\tbir kesit saðlama.\n\
+-no-toolbar\t\t\tbir araç çubuðu saðlama.\n\
+-rc <string>\t\t\tulternatif kaynak dosyasýný kullan.\n\
+-version\t\t\tsürümü görüntüle ve çýk.\n\
+-info\t\t\t\tyararlý bilgi görüntüle.\n\
+-list-commands\t\t\tbütün geçerli tuþ komutlarýný listele.\n\
+-sync\t\t\t\thata ayýklama için X sunucusu ile eþzamanla.\n\
+-log <filename>\t\t\tgünlük çýktýsýný dosyaya yaz.\n\
+-help\t\t\t\tbu yardým ekranýný göster ve çýk.\n\n
+14 uyarý: çevre deðiþkeni 'DISPLAY' ayarlanamýyor
+
+$set 20 #layers
+
+1 Panelin Üstü
+2 Alt
+3 Masaüstü
+4 Panel
+5 Normal
+6 Üst
+
